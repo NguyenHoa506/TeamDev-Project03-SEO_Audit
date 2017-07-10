92c3bf07bd4d93e15b50f8f43361a29db6901fef
nk\Hoa Oanh <1411270@hcmut.edu.vn>
2017-07-10 17:31:03 +0700

    chinh sua

--- page-audit.php
+++ page-audit.php
@@ -175,40 +175,40 @@
         </div>
 
         <div class="col-md-12 col-xs-12 div-head">
-            <div class="row" align="center">
+            <div class="row div-cach" align="center">
                 <h2>Mục Đích</h2>
             </div>
             <div class="row div-cach">
                 <div class="col-md-10 col-md-offset-1 col-xs-12">
                     <div class="col-md-12 col-xs-12">
-                        <div class="col-md-4 col-xs-12">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
                             <div class="col-md-12">
                                 <div style="margin:0 auto ;border-radius:50%; background-color:white; height:150px; width:150px; padding:15px; color:#fff; position:relative">
                                     <img class="div-imgh" src="<?php bloginfo('template_directory');?>/image/graph-analysis.png">
                                 </div>
-                                <div>
+                                <div class="div-cach">
                                     <h4>Có cái nhìn khách quan từ bên ngoài về tình trạng SEO hiện tại</h4>
                                 </div>
                             </div>
                         </div>  
 
-                        <div class="col-md-4 col-xs-12">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
                             <div class="col-md-12">
                                 <div style="margin:0 auto ;border-radius:50%; background-color:white; height:150px; width:150px; padding:15px; color:#fff; position:relative">
                                     <img class="div-imgh" src="<?php bloginfo('template_directory');?>/image/refresh-button.png">
                                 </div>
-                                <div>
+                                <div class="div-cach">
                                      <h4>Cập nhập những thay đổi mới nhất từ Google & cải tiến hệ thống SEO hiện tại</h4>                       
                                 </div>
                             </div>
                         </div>
 
-                        <div class="col-md-4 col-xs-12">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
                             <div class="col-md-12">
                                 <div style="margin:0 auto ;border-radius:50%; background-color:white; height:150px; width:150px; padding:15px; color:#fff; position:relative">
                                     <img class="div-imgh" src="<?php bloginfo('template_directory');?>/image/person-of-a-call-center-in-communication-with-headphones.png">
                                 </div>
-                                <div>
+                                <div class="div-cach">
                                     <h4>Được hỗ trợ tư vấn chuyên sâu về từng vấn đề của SEO mà ít có team nào có thể bao quát hết được</h4>
                                 </div>
                             </div>
